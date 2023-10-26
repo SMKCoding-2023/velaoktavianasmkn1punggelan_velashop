90982b160365ff4ba6da961d68afddc1d0d7210a
velaokta <velaoktaviana85@gmail.com>
2023-10-26 15:57:04 +0700

    upload

---
+++ composables/constants/category.ts
@@ -0,0 +1,34 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Matahari",
+    },
+    {
+    id: 2,
+    name: "Mawar",
+    },
+    {
+    id: 3,
+    name: "Putih",
+    },
+    {
+    id: 4,
+    name: "Wadah Bunga",
+    },
+    {
+    id: 5,
+    name: "Pot Cantik",
+    },
+    {
+    id: 6,
+    name: "Alat Siram Unik",
+    },
+    {
+    id: 7,
+    name: "Rak Bunga",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="home">
+      <div class="header">  
+      </div>
+      <div class="content">
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+  }
+  </script>
+  
+  <style scoped>
+  /* Gaya khusus untuk halaman Home */
+  .home {
+    text-align: center;
+    padding: 20px;
+    background-image: url('/images/baground.jpg');
+    background-size: cover;
+    color: white;
+  }
+  
+  .header h1 {
+    font-size: 2.5em;
+    margin-bottom: 10px;
+  }
+  
+  .header p {
+    font-size: 1.5em;
+    color: #777;
+  }
+  
+  .content h2 {
+    font-size: 1.8em;
+    margin-top: 20px;
+  }
+  
+  .content ul {
+    list-style-type: disc;
+    font-size: 1.2em;
+    text-align: left;
+    padding-left: 20px;
+  }
+  
+  /* Gaya tambahan untuk elemen list */
+  .content li {
+    margin: 10px 0;
+  }
+  
+  /* Gaya untuk tautan jika Anda ingin menambahkan tautan navigasi */
+  /* .content a {
+    color: #007BFF;
+    text-decoration: underline;
+  } */
+  </style>
